9b4af5a09443a8d497b2d4fade63e800da989574
P0lskay <gg.lol.2264@gmail.com>
2020-10-25 23:16:25 +0300

    Доделал вывод товара в админке

--- resources/views/shop/admin/watches/index.blade.php
+++ resources/views/shop/admin/watches/index.blade.php
@@ -1,13 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<table>
-    @foreach($paginator as $item)
-    <tr>
-        <td>{{$item->id}}</td>
-        <td>{{$item->title}}</td>
-        <td>{{$item->producer}}</td>
-    </tr>
-    @endforeach
-</table>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <nav class="navbar navbar-toogleable-md navbar-light bg-faded">
+                <a href="{{route('shop.admin.watches.create')}}" class="btn btn-primary">Написать</a>
+            </nav>
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Заголовок</th>
+                                <th>Категория</th>
+                                <th>Производитель</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($paginator as $watch)
+                            @php /** @var \App\Models\BlogPost $item */ @endphp
+                            <td>{{$watch->id}}</td>
+                            <td>
+                                <a href="{{route('shop.admin.watches.edit', $watch->id)}}">
+                                    {{$watch->title}}
+                                </a>
+                            </td>
+                            <td>{{$watch->category->id}}</td>
+                            <td>{{$watch->producer}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    @if($paginator->total() > $paginator->count())
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    {{$paginator->links()}}
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+</div>
 @endsection
