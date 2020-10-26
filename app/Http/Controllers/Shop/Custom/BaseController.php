5551dc735166a0adc734a78a7c3c8035d20f22c5
P0lskay <gg.lol.2264@gmail.com>
2020-10-26 23:42:06 +0300

    Сделан контроллер для показа часов пользователю

---
+++ app/Http/Controllers/Shop/Custom/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Shop\Custom;
+
+use App\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+    public function __construct()
+    {
+    }
+}
---
+++ app/Http/Controllers/Shop/Custom/ShopWatchController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Shop\Custom;
+
+use App\Repositories\ShopWatchRepository;
+use Illuminate\Http\Request;
+
+class ShopWatchController extends BaseController
+{
+    private $shopWatchRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->shopWatchRepository = app(ShopWatchRepository::class);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $paginator = $this->shopWatchRepository->getAllWithPaginate();
+        return view('shop.custom.main.index', compact($paginator, 'paginator'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    }
+}
